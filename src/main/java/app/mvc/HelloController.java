91ed78308f2186cb60ba87f1c665fbdaa9aacd67
“Rodion <rdblkv@gmail.com>
2022-11-25 18:41:06 +0300

    Initial commit

---
+++ src/main/java/app/mvc/HelloController.java
@@ -0,0 +1,13 @@
+package app.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String hello(){
+        return "hello_world";
+    }
+}
