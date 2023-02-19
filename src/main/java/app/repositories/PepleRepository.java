eab2c1dda007192f2c96ce7b5446fbed0b85922f
nerodya <114946150+nerodya@users.noreply.github.com>
2023-02-19 18:49:35 +0300

    Delete src/main/java/app directory

--- src/main/java/app/repositories/PepleRepository.java
+++
@@ -1,7 +0,0 @@
-package app.repositories;
-
-import app.models.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PepleRepository extends JpaRepository<Person, Integer> {
-}
